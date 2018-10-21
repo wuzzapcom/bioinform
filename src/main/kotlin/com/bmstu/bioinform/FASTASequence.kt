60e059fd03799d7a94f9213e390879ad88478e5c
Vladimir Lapatin <vladimir.lapatin@acronis.com>
2018-10-21 03:18:29 +0300

    Improved output

--- src/main/kotlin/com/bmstu/bioinform/FASTASequence.kt
+++ src/main/kotlin/com/bmstu/bioinform/FASTASequence.kt
@@ -25,6 +25,8 @@ class FASTASequence(private val name: String, private val sequence: String): Ite
 
     companion object {
 
+        const val OUTPUT_STRING_LENGTH = 80
+
         fun parse(text: String, disableCheck: Boolean): FASTASequence {
             if (text[0] != '>') throw RuntimeException("Not FASTA")
             var name = ""
@@ -54,25 +56,68 @@ class FASTASequence(private val name: String, private val sequence: String): Ite
 
             val str1 = s1.modified.toString()
             val str2 = s2.modified.toString()
-            val res = StringBuilder()
 
+            val r1 = StringBuilder()
             for (ch in str1) {
-                res.append("$ch ")
+                r1.append("$ch ")
             }
-            res.append("\n")
+
+            val r2 = StringBuilder()
             for (i in 0 until str1.length) {
                 if (str1[i] == str2[i]){
-                    res.append("* ")
+                    r2.append("* ")
                 } else if (str1[i] == Score.GAP_SYMBOL || str2[i] == Score.GAP_SYMBOL) {
-                    res.append("  ")
+                    r2.append("  ")
                 } else {
-                    res.append("| ")
+                    r2.append("| ")
                 }
             }
-            res.append("\n")
+
+            val r3 = StringBuilder()
             for (ch in str2) {
-                res.append("$ch ")
+                r3.append("$ch ")
             }
+
+            var i = 0
+            var j = 0
+
+            val ar1 = arrayListOf<String>()
+            while (i < r1.length) {
+                if (i != 0 && i % OUTPUT_STRING_LENGTH == 0) {
+                    ar1.add(r1.substring(j * OUTPUT_STRING_LENGTH,
+                            if ((j+1) * OUTPUT_STRING_LENGTH < r1.length) ++j* OUTPUT_STRING_LENGTH else r1.length))
+                }
+                i++
+            }
+            i = 0
+            j = 0
+            val ar2 = arrayListOf<String>()
+            while (i < r2.length) {
+                if (i != 0 && i % OUTPUT_STRING_LENGTH == 0) {
+                    ar2.add(r2.substring(j * OUTPUT_STRING_LENGTH,
+                            if ((j+1) * OUTPUT_STRING_LENGTH < r2.length) ++j*OUTPUT_STRING_LENGTH else r2.length))
+                }
+                i++
+            }
+            i = 0
+            j = 0
+            val ar3 = arrayListOf<String>()
+            while (i < r3.length) {
+                if (i != 0 && i % OUTPUT_STRING_LENGTH == 0) {
+                    ar3.add(r3.substring(j * OUTPUT_STRING_LENGTH,
+                            if ((j+1) * OUTPUT_STRING_LENGTH < r3.length) ++j*OUTPUT_STRING_LENGTH else r3.length))
+                }
+                i++
+            }
+
+            val res = StringBuilder()
+            for (i in 0 until ar1.size) {
+                res.append("${ar1[i]}\n")
+                res.append("${ar2[i]}\n")
+                res.append("${ar3[i]}\n")
+                res.append("\n")
+            }
+
             return res.toString()
         }
     }
